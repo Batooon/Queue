c0cceb82e209d989cca6f5b6f13afcc49039f27d
Batooon <rozum.ant@gmail.com>
2019-04-26 18:11:20 +0300

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Node<T>
+    {
+        public Node<T> next;
+        public T value;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ Que.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Que<T>
+    {
+        Node<T> first;
+        Node<T> last;
+        public void Enqueue(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if (first == null)
+            {
+                last = newNode;
+                first = last;
+            }
+            else
+            {
+                last.next = newNode;
+                last = last.next;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new IndexOutOfRangeException();
+
+            T value = first.value;
+
+            first = first.next;
+
+            return value;
+        }
+    }
+}
